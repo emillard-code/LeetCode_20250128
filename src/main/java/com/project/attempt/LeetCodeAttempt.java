e8316de4f378d15bec9d668856a3e6752349d110
Emerson Millard <emillard.code@gmail.com>
2025-01-28 16:37:52 -0800

    Added comments to challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -12,10 +12,14 @@ public class LeetCodeAttempt {
 
     }
 
+    // This method returns the maximum number of fish you can catch from this grid.
     public static int maximumNumberOfFishInAGrid(int[][] grid) {
 
         int maximumFish = 0;
 
+        // We loop through every index and call a helper method that will calculate the maximum number
+        // of fish that can be caught if we started from that index. Whatever value turns out to be the
+        // highest after looping through everything is the value that we ultimately return at the end.
         for (int i = 0; i < grid.length; i++) {
 
             for (int j = 0; j < grid[0].length; j++) {
@@ -31,8 +35,12 @@ public class LeetCodeAttempt {
 
     }
 
+    // A helper method that calculates the highest number of fish that can be
+    // caught if we start from a certain index. It is a recursive function.
     private static int travelGrid(int[][] grid, int x, int y) {
 
+        // We create a new copy of the grid every time we call this method to prevent
+        // any unwanted behaviors in the grid between its various method calls.
         int[][] newGrid = new int[grid.length][grid[0].length];
 
         for (int i = 0; i < grid.length; i++) {
@@ -41,14 +49,22 @@ public class LeetCodeAttempt {
             }
         }
 
+        // We set the current coordinates to 0 to indicate that everything has been fished
+        // and that it is a cell that we may no longer return to.
         newGrid[x][y] = 0;
         int fish = grid[x][y];
 
+        // We have four int primitives each pertaining to a cardinal direction. They will
+        // represent the amount of fish that can be obtained if the fisher moves in that direction.
+        // We set them to a default of -1 which will remain that way if the cell in that particular
+        // direction is not a valid path (either out of bounds of the array or is a land cell).
         int east = -1;
         int west = -1;
         int north = -1;
         int south = -1;
 
+        // For each direction, we make a recursive call to this function if it is a valid path
+        // to go on. We record the number of fish obtained that each direction would result in.
         if (x + 1 < grid.length) {
             if (newGrid[x + 1][y] != 0) {
                 south = travelGrid(newGrid, x + 1, y);
@@ -73,10 +89,14 @@ public class LeetCodeAttempt {
             }
         }
 
+        // If none of the directions are valid, we simply return the value
+        // of the current number of fish that have been accumulated.
         if (east == -1 && west == -1 && north == -1 && south == -1) {
             return fish;
         }
 
+        // Otherwise, we calculate which direction results in the highest number of
+        // fish obtained, and increment that by the number of fish in this cell.
         return fish + Math.max(east, Math.max(west, Math.max(north, south)));
 
     }
